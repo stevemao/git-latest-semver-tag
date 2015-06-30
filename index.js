17409904fe28c01bee651a296d0447d00f23e744
Steve Mao <maochenyan@gmail.com>
2015-06-30 15:35:20 +1000

    cache cmd and better var names

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 var exec = require('child_process').exec;
 var semverValid = require('semver').valid;
 var regex = /tag:\s*(.+?)[,\)]/gi;
+var cmd = 'git log --date-order --tags --simplify-by-decoration --pretty=format:"%d"';
 
 module.exports = function(callback) {
-  exec('git log --date-order --tags --simplify-by-decoration --pretty=format:"%d"', function(err, data) {
+  exec(cmd, function(err, data) {
     if (err) {
       callback(err);
       return;
@@ -12,12 +13,12 @@ module.exports = function(callback) {
 
     var hasSemver = false;
 
-    data.split('\n').some(function(raw) {
-      var tag;
-      while (tag = regex.exec(raw)) {
-        var ver = tag[1];
-        if (semverValid(ver)) {
-          callback(null, ver);
+    data.split('\n').some(function(decorations) {
+      var match;
+      while (match = regex.exec(decorations)) {
+        var tag = match[1];
+        if (semverValid(tag)) {
+          callback(null, tag);
           hasSemver = true;
           return true;
         }
